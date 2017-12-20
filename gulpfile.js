e690402fbe4792416ea8c68390c7c1b0d0f8ae0f
nataliecarstens <nataliecarstens@me.com>
2017-12-20 21:13:42 +0100

    coffee script

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
 
 gulp.task('copy', function () {
   gulp.src('index.html')
@@ -16,4 +17,11 @@ gulp.task('sass', function() {
   .pipe(sass({style: 'expanded'}))
     .on('error', gutil.log)
   .pipe(gulp.dest('assets'))
+});
+
+gulp.task('coffee', function () {
+  gulp.src('scripts/hello.coffee')
+  .pipe(coffee({bare:true})
+    .on('error', gutil.log))
+  .pipe(gulp.dest('scripts'))
 });
---
+++ scripts/hello.coffee
@@ -0,0 +1,3 @@
+square = (x) -> x * x
+cube = (x) -> square(x) * x
+console.log("Hellow World from Coffee Script")
