5365399cc111773277930bd40c7680430c2e02dc
nataliecarstens <nataliecarstens@me.com>
2017-12-20 20:59:50 +0100

    sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
 
 gulp.task('copy', function () {
   gulp.src('index.html')
+  .pipe(gulp.dest('assets'))
+});
+
+gulp.task('log', function () {
+  gutil.log('--first log task---')
+});
+
+gulp.task('sass', function() {
+  gulp.src('styles/main.scss')
+  .pipe(sass({style: 'expanded'}))
+    .on('error', gutil.log)
   .pipe(gulp.dest('assets'))
 });
